56f3588a1f55b497d95a0aa2de0710b4210c62cc
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-20 12:16:07 -0400

    before changes

---
+++ instructor/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1;
+
+var service = new EmployeeLookup();
+
+var emp = service.GetEmployeeById(1);
+
+if (emp != null)
+{
+   
+
+    Console.WriteLine($"That employee is {emp.Name} and they make {emp.Salary:c}");
+}
+else
+{
+    Console.WriteLine("No Employee With That Id");
+}
+
+var emp1 = new Employee(1, "Jill Jones", 120_000);
+var emp2 = new Employee(1, "Jill Jones", 120_000);
+
+
+if(emp1 == emp2)
+{
+    Console.WriteLine("They are the same!");
+} else
+{
+    Console.WriteLine("They Are different!");
+
+}
+
+var upgradedEmp1 = emp1 with { Salary = 220_000 };
+
+Console.WriteLine(emp1 );
+Console.WriteLine(upgradedEmp1);
---
+++ instructor/DemoApi/HrClass.cs
@@ -0,0 +1,5 @@
+﻿namespace DemoApi.Hr; // File Scoped Namespaces
+
+public class Employee { }
+
+public class Contractor { }
